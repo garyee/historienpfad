8e9ed51bb69829ff898dc12e90c342b7d33eba9d
garyee <garyee@gmx.de>
2018-07-11 16:36:05 +0200

    Get Paths by Locations

--- historienpfad/src/components/google-map/google-map.ts
+++ historienpfad/src/components/google-map/google-map.ts
@@ -1,11 +1,11 @@
 import {Component, Input, Renderer2, ElementRef, Inject} from '@angular/core';
-import { DOCUMENT } from '@angular/platform-browser';
-import { Plugins } from '@capacitor/core';
+import {DOCUMENT} from '@angular/platform-browser';
+import {Plugins} from '@capacitor/core';
 import {} from '@types/googlemaps';
 import {GeoService} from "../../../services/database/geo.service";
-import {PointListService} from "../../../services/database/point-list.service";
+import {PointService} from "../../../services/database/point.service";
 //import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng } from './googlemaps';
-const { Geolocation, Network } = Plugins;
+const {Geolocation, Network} = Plugins;
 import moment from "moment";
 import {AuthService} from "../../../services/auth.service";
 
@@ -30,13 +30,13 @@ export class GoogleMapComponent {
     private element: ElementRef,
     @Inject(DOCUMENT) private _document,
     private geo: GeoService,
-    private point: PointListService,
+    private point: PointService,
     private auth: AuthService,
-  ){
+  ) {
 
   }
 
-  ngOnInit(){
+  ngOnInit() {
 
     this.init().then((res) => {
       this.retrievePaths();
@@ -75,11 +75,11 @@ export class GoogleMapComponent {
 
     return new Promise((resolve, reject) => {
 
-      if(!this.mapsLoaded){
+      if (!this.mapsLoaded) {
 
         Network.getStatus().then((status) => {
 
-          if(status.connected){
+          if (status.connected) {
 
             this.injectSDK().then((res) => {
               resolve(true);
@@ -89,11 +89,11 @@ export class GoogleMapComponent {
 
           } else {
 
-            if(this.networkHandler == null){
+            if (this.networkHandler == null) {
 
               this.networkHandler = Network.addListener('networkStatusChange', (status) => {
 
-                if(status.connected){
+                if (status.connected) {
 
                   this.networkHandler.remove();
 
@@ -116,7 +116,7 @@ export class GoogleMapComponent {
         }, (err) => {
 
           // NOTE: navigator.onLine temporarily required until Network plugin has web implementation
-          if(navigator.onLine){
+          if (navigator.onLine) {
 
             this.injectSDK().then((res) => {
               resolve(true);
@@ -151,7 +151,7 @@ export class GoogleMapComponent {
       let script = this.renderer.createElement('script');
       script.id = 'googleMaps';
 
-      if(this.apiKey){
+      if (this.apiKey) {
         script.src = 'https://maps.googleapis.com/maps/api/js?key=' + this.apiKey + '&callback=mapInit';
       } else {
         script.src = 'https://maps.googleapis.com/maps/api/js?callback=mapInit';
@@ -169,7 +169,7 @@ export class GoogleMapComponent {
 
       Geolocation.getCurrentPosition().then((position) => {
         this.watchPosition();
-        if(position!=null) {
+        if (position != null) {
           let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
           let mapOptions = {
             center: latLng,
@@ -180,8 +180,8 @@ export class GoogleMapComponent {
           resolve(true);
 
           this.setMe(position.coords.latitude, position.coords.longitude);
-        }else{
-          let latLng = new google.maps.LatLng(50.8389047,12.927764);
+        } else {
+          let latLng = new google.maps.LatLng(50.8389047, 12.927764);
           let mapOptions = {
             center: latLng,
             zoom: 15
@@ -198,20 +198,22 @@ export class GoogleMapComponent {
       });
     });
   }
+
   /*
   Marker management
    */
-  public delMarker(markerid: number): boolean{
-    if(this.markers[markerid]!=undefined) {
+  public delMarker(markerid: number): boolean {
+    if (this.markers[markerid] != undefined) {
       this.markers[markerid].remove();
       delete(this.markers[markerid]);
       return true;
     }
     return false;
   };
+
   public addMarker(markerid: number, lat: number, lng: number, title: string): void {
     let latLng = new google.maps.LatLng(lat, lng);
-    if(this.markers[markerid]!=undefined){
+    if (this.markers[markerid] != undefined) {
       this.markers[markerid].setPosition(latLng);
       return;
     }
@@ -223,16 +225,16 @@ export class GoogleMapComponent {
       //draggable: true,
       title: title,
     });
-    this.markers[markerid]=marker;
+    this.markers[markerid] = marker;
   }
 
-  public getMarkercount(){
+  public getMarkercount() {
     return this.markers.length;
   }
 
   public setMe(lat: number, lng: number): void {
     let latLng = new google.maps.LatLng(lat, lng);
-    if(this.me!=undefined){
+    if (this.me != undefined) {
       this.me.setPosition(latLng);
       return;
     }
@@ -242,60 +244,60 @@ export class GoogleMapComponent {
       animation: 0,
       position: latLng,
     });
-    this.me=marker;
+    this.me = marker;
   }
-  public getPosition(): Promise<{lat:number,lng:number}>{
-      return Geolocation.getCurrentPosition().then((position) => {
-        if(position!=null) {
-          this.lat=position.coords.latitude;
-          this.lng=position.coords.longitude;
-          return {lat:this.lat, lng:this.lng};
-        }
-      }, (err) => {
-        this.lat=-1;
-        this.lng=-1;
-        return {lat:this.lat, lng:this.lng};
-      });
+
+  public getPosition(): Promise<{ lat: number, lng: number }> {
+    return Geolocation.getCurrentPosition().then((position) => {
+      if (position != null) {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+        return {lat: this.lat, lng: this.lng};
+      }
+    }, (err) => {
+      this.lat = -1;
+      this.lng = -1;
+      return {lat: this.lat, lng: this.lng};
+    });
   }
+
   /*
    Position Callback
    */
   private watchPosition() {
     this.watchdog = Geolocation.watchPosition({}, (position, err) => {
-      if(this.map!=undefined) {
+      if (this.map != undefined) {
         if (position != undefined) {
           let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
           this.map.setCenter(latLng);
           this.setMe(position.coords.latitude, position.coords.longitude);
-        }else{
+        } else {
           console.log("Position Error")
         }
-      }else{
+      } else {
         console.log("Map not initialized");
       }
     });
   }
 
-  public retrievePaths(){
+  public retrievePaths() {
     let center = this.map.getCenter();
-    this.geo.getLocations(100,[center.lat(),center.lng()],(key, location, distance)=>{
-      this.point.getPoint(key,(res)=>{
-        this.addMarker(this.getMarkercount(),location[0],location[1],(res.email+' '+ moment(res.ts).format('YYYY-MM-DD h:mm:ss')));
-      });
-    });
+    // this.geo.getLocations(100, [center.lat(), center.lng()], (key, location, distance) => {
+    //   this.point.getPoint(key, (res) => {
+    //     this.addMarker(this.getMarkercount(), location[0], location[1], (res.email + ' ' + moment(res.ts).format('YYYY-MM-DD h:mm:ss')));
+    //   });
+    // });
   }
-  public setNewMarker(){
+
+  public setNewMarker() {
     let center = this.map.getCenter();
     this.addMarker(
       this.markers[this.getMarkercount()],
       center.lat(),
-      center.lng(),"marker");
-
-    const key=new Date().getTime()+'';
-    this.geo.setLocation(key, [center.lat(),center.lng()]);
-    this.point.addPoint(key,{
-          name:"Test"+Math.round(Math.random()*10),
-          email:this.auth.getEmail(),
-          ts:new Date().getTime()});
+      center.lng(), "marker");
+    this.point.addPoint({
+      name: "Test" + Math.round(Math.random() * 10),
+      coords: [center.lat(), center.lng()]
+    });
   }
 }
